1602a19dfe5570ecce5b13023ea84f7a757d075f
Pablq <pabloignaciophilipps@gmail.com>
2015-02-08 21:11:21 -0600

    removed remnants of old React.js tutorial. Those fragments were moved to their own directory. currently this application is as minimal as possible.

--- server.js
+++ server.js
@@ -1,102 +1,51 @@
-var http = require("http");
-var path = require("path");
-var fs = require("fs");
-var qs = require("querystring");
+var http = require("http"),
+    path = require("path"),
+    fs = require("fs"),
+    port = Number(process.env.PORT || 5000);
 
 function requestHandler(req, res) {
-  var filePath = "." + req.url;
-  if (req.method === "POST" && filePath === "./comments.json") {
-    fs.readFile(filePath, function (err, content) {
-      if (err) {
-        console.log("there was an error reading ./comments.json", err);
-        res.writeHead(500);
-        res.end();
-      } else {
-        var allComments = JSON.parse(content);
-        var body = "";
-        req.setEncoding("utf8");
-        req.on("data", function (data) {
-          body += data; 
-        });
-        req.on("end", function () {
-          allComments.push(qs.parse(body));
-          fs.writeFile("comments.json", JSON.stringify(allComments), function (err) {
-            if (err) {
-              console.log("there was an error writing comments.json", err); 
-              res.writeHead(500);
-              res.end();
-            } else {
-              var time = new Date();
-              fs.appendFile("nukes.txt", time.toString() + "\n", function (err) {
-                  // Do nothing
-              });
-              res.writeHead(200, { "Content-Type" : "text/json" });
-              res.end(JSON.stringify(allComments));
-            }
-          });
-        });
-      }
-    });
-  } else if (req.method === "DELETE" && filePath === "./comments.json") { 
-    var emptyComments = [];
-    fs.writeFile("comments.json", JSON.stringify(emptyComments), function (err) {
-      if (err) {
-        console.log("there was an error nuking comments.json", err);
-        res.writeHead(500);
-        res.end();
-      } else {
-        res.writeHead(200, { "Content-Type" : "text/json" });
-        res.end("[]");
-      }
-    }); 
-  } else if (req.method === "GET") {
+
+    var filePath = "." + req.url;
+
     if (filePath === "./") {
       filePath = "./index.html";
     }
+
     var extname = path.extname(filePath);
-    var contentType = "text/html";
 
     switch (extname) {
-      case ".gif":
-        contentType = "image/gif";
-      case ".png":
-        contentType = "image/png";
-        break;
-      case ".js":
-        contentType = "text/javascript";
-        break;
-      case ".css":
-        contentType = "text/css";
-        break;
+        case ".gif":
+            contentType = "image/gif";
+            break;
+        case ".png":
+            contentType = "image/png";
+            break;
+        case ".js":
+            contentType = "text/javascript";
+            break;
+        case ".css":
+            contentType = "text/css";
+            break;
+        default:
+            contentType = "text/html";
     }
 
     fs.exists(filePath, function(exists) {
-      if (exists) {
-        fs.readFile(filePath, function(err, content) {
-          if (err) {
-            console.log("there was an error when serving file");
-            res.writeHead(500);
+        if (exists) {
+            fs.readFile(filePath, function(err, content) {
+                if (err) {
+                    res.writeHead(500);
+                    res.end();
+                } else {
+                    res.writeHead(200, { "Content-Type" : contentType });
+                    res.end(content);
+                }
+            });
+        } else {
+            res.writeHead(404);
             res.end();
-          }
-          else {
-            console.log("serving " + filePath);
-            res.writeHead(200, { "Content-Type" : contentType });
-            res.end(content);
-          }
-        });
-      } else {
-        console.log("file was not found");
-        res.writeHead(404);
-        res.end();
-      }
+        }
     });
-  } else {
-    res.writeHead(406);
-    res.end();
-  }
 }
 
-var port = Number(process.env.PORT || 5000);
-
 http.createServer(requestHandler).listen(port);
-console.log("listening on port "+ port);

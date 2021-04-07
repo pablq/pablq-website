8396065fa33163c8afd9b7da3bcfe33aefcc17be
Pablo Philipps <pabloignaciophilipps@gmail.com>
2021-04-07 00:11:03 -0500

    Adds redirect to force visitors to use https.

--- router/index.js
+++ router/index.js
@@ -10,22 +10,26 @@ module.exports = (() => {
         };
 
     return (req, res) => {
-        
-        var path;
+
+        if (req.headers["x-forwarded-proto"] !== "https") {
+            res.writeHead(301, {
+                "Location": "https://" + req.headers.host + req.url
+            }); 
+            res.end("Moved Permanently\n");
+            return;
+        } 
 
         if (req.method !== "GET") {
-
             res.writeHead(501, { "Content-Type" : "text/plain" });
             res.end("CANNOT " + req.method + "\n");
+            return;
+        }
 
+        var path = req.url.match(/\w+/g);
+        if (path && routes[path[0]]) {
+            routes[path[0]](path.slice(1), req, res);
         } else {
-            
-            path = req.url.match(/\w+/g);
-
-            if (path && routes[path[0]])
-                routes[path[0]](path.slice(1), req, res);
-            else
-                routes["def"](req, res);
+            routes["def"](req, res);
         }
     };
 })();
